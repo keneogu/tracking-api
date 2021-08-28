0664fbdd60b9747205edc8da1f6f9767463e025a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-08-28 21:30:39 +0100

    Add users rspec tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'rspec/rails'
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 
